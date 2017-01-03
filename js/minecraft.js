d48e46166f1176919873888c4fc9310f884676de
Valentine Ganansia <valentineganansia@gmail.com>
2017-01-03 16:07:30 +0200

    including bootstrap

--- js/minecraft.js
+++ js/minecraft.js
@@ -23,8 +23,40 @@ minecraft.createButton = function(){
     btn.click(minecraft.newGame);
 }
 
-minecraft.newGame = function(){
+minecraft.newGame = function(){ // set the game board. 
     alert('test');
 }
 
 minecraft.createButton();
+
+//Fonction of the menu that have to be on pause when the main is landing, and on play when the main is gameboard. : 
+
+
+
+// pickaxe() : Doit comparer si il s'agit d'un background pierre, Si oui attrape le background. 
+
+
+
+//shovel(); idem sauf qu'il s'agit de la terre.
+
+
+//axe(); idem sauf qu'il s'agit du bois et des buissons. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
