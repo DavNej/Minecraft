41a29ad74fa173db57b4eb1d78cd8140edcd7cd4
DavNej <david.nejar@gmail.com>
2017-01-03 13:30:56 +0200

    basic html,css,js & jQuery linked and working

---
+++ js/minecraft.js
@@ -0,0 +1,18 @@
+alert('Ca marche :)\nCa va?');
+
+var minecraft = {};
+/*
+Ca c'est un NameSpace. A partir de cette ligne, toute nouvelle variable ou fonction doit commencer par minecraft.nomDeLaChose
+
+______ Declaration d'une nouvelle fonction: ______
+
+funciton minecraft.nomDeLaFonction{
+    console.log('youpi');
+}
+
+______ Declaration d'une nouvelle variable: ______
+
+minecraft.nouvelleVariable = 'NameSpace is easy';
+
+meme plus besoin de lui dire var :)
+*/
