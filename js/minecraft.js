82a341a8fb33284402cf01acacd5025cbbcbab5e
DavNej <david.nejar@gmail.com>
2017-01-04 12:28:29 +0200

    matrix

--- js/minecraft.js
+++ js/minecraft.js
@@ -22,47 +22,58 @@ minecraft.createButton = function(){
     btn.text("START");
     $("#main").append(btn);
     btn.click(minecraft.newGame);
-}
+};
 
 minecraft.newGame = function(){ // set the game board.
-		    $('#logo').hide();
-		    $('.myButton').hide();
-		    $('#main').css({'background-image':'none', 'background-color':'rgb(73, 190, 203)'});
-		    minecraft.gamePlay = true;
+	$('#logo').hide();
+    $('.myButton').hide();
+	$('#main').css({'background-image':'none', 'background-color':'#3b3d3f', 'line-height': '0'});
+    $('#world').css({"display" : "block"});
+    minecraft.gamePlay = true;
+    minecraft.initWorld();
 }
-/*onclick="newGame();location.reload();" ou quelque chose du genre a faire dans le html */
 
+minecraft.initWorld = function(){
+    minecraft.createMatrix();
+    minecraft.initMatrix();
+    //minecraft.updateBoard();
+    
+
+}
+
+minecraft.createMatrix = function(){
+    minecraft.matrix = Array(20);
+    for(var i = 0; i < minecraft.matrix.length; i++){
+        minecraft.matrix[i] = Array(20);
+    }
+    //intialization
+    for(var i = 0; i < minecraft.matrix.length; i++){
+        for(var j = 0; j < minecraft.matrix.length; j++){
+            minecraft.matrix[i][j] = "";
+        }
+    }
+}
+
+minecraft.initMatrix = function(){
+    for(var i = 0; i < minecraft.matrix.length; i++){
+        for(var j = 0; j < minecraft.matrix.length; j++){
+            var block = $('<div>');
+            block
+                .addClass('empty')
+                .data("i", i)
+                .data("j", j);
+            
+            $('#world').append(block);
+        }
+    }
+}
 
 minecraft.createButton();
 
 //Fonction of the menu that have to be on pause when the main is landing, and on play when the main is gameboard. : 
 
-
-
 // pickaxe() : Doit comparer si il s'agit d'un background pierre, Si oui attrape le background. – for mining rocks
-
-
 
 //shovel(); idem sauf qu'il s'agit de la terre. -for digging dirt
 
-
 //axe(); idem sauf qu'il s'agit du bois et des buissons. – for cutting trees
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
